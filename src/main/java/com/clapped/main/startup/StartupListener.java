db4d97d62ce62ce4107c5ba60c2e8da781a07cf7
Jai Gandhi <jai.2002.gandhi@gmail.com>
2025-12-26 17:24:01 +0000

    forgor ab this guy

--- src/main/java/com/clapped/main/startup/StartupListener.java
+++ src/main/java/com/clapped/main/startup/StartupListener.java
@@ -1,6 +1,7 @@
 package com.clapped.main.startup;
 
-import com.clapped.main.model.ProcessResult;
+import com.clapped.main.messaging.events.SettingsEvent;
+import com.clapped.main.messaging.producer.SettingsEventProducer;
 import com.clapped.main.service.GameState;
 import com.clapped.pokemon.service.PokemonService;
 import io.quarkus.runtime.StartupEvent;
@@ -9,50 +10,38 @@ import jakarta.enterprise.event.Observes;
 import jakarta.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
 
-import static com.clapped.main.messaging.events.EventType.GAME_EVENT;
-import static com.clapped.main.messaging.events.GameEvtType.GENERATION_CHANGE;
-import static com.clapped.main.messaging.events.GameEvtType.LEVEL_CHANGE;
+import java.util.LinkedList;
 
 @Slf4j
 @ApplicationScoped
 public class StartupListener {
 
     private final GameState gameState;
-    private final GameEventProducer gameEventProducer;
+    private final SettingsEventProducer settingsEventProducer;
     private final PokemonService pokemonService;
 
     @Inject
-    public StartupListener(final GameState gameState, final GameEventProducer gameEventProducer, final PokemonService pokemonService) {
+    public StartupListener(final GameState gameState, final SettingsEventProducer settingsEventProducer, final PokemonService pokemonService) {
         this.gameState = gameState;
-        this.gameEventProducer = gameEventProducer;
+        this.settingsEventProducer = settingsEventProducer;
         this.pokemonService = pokemonService;
     }
 
     void init(@Observes final StartupEvent startupEvent) {
         log.info("\u001B[32mApplication Version: {}\u001B[0m", System.getenv("BUILD_VERSION"));
         sendInitialGameInfo();
-        populateTypes();
+        pokemonService.populateAllTypes();
     }
 
     private void sendInitialGameInfo() {
-        gameEventProducer.sendGameEvent(new GameEvent(
-                System.currentTimeMillis(),
-                GAME_EVENT,
-                GENERATION_CHANGE,
+        final LinkedList<String> logMsgs = new LinkedList<>();
+        logMsgs.add("Initialised Pokemon Generation to " + gameState.getPokemonGen());
+        logMsgs.add("Initialised Pokemon Level to " + gameState.getPokemonLevel());
+        settingsEventProducer.sendSettingsEvent(new SettingsEvent(
                 gameState.getPokemonGen().getNumber(),
-                ProcessResult.success("Initialised Pokemon Generation as " + gameState.getPokemonGen())
-        ));
-        gameEventProducer.sendGameEvent(new GameEvent(
-                System.currentTimeMillis(),
-                GAME_EVENT,
-                LEVEL_CHANGE,
                 gameState.getPokemonLevel(),
-                ProcessResult.success("Initialised Pokemon Level as " + gameState.getPokemonLevel())
+                logMsgs
         ));
-    }
-
-    private void populateTypes() {
-        pokemonService.populateAllTypes();
     }
 
 }
